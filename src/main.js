3d1f1dffb3793d66e9a9e150eac7c698e22755da
Sonorously <17600425115@163.com>
2021-08-06 18:26:20 +0800

    目前路由的菜单展示已经完成

--- src/main.js
+++ src/main.js
@@ -8,4 +8,9 @@ import './assets/style/index.scss'
 import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
 
+//首页路由页面加载
+import dynamic from "@/router/dynamic";
+//dynamic获取到了页面路由配置页面，直接在此给store中的state赋值，就可以在页面中显示MENU菜单
+store.commit('SET_MENU',dynamic);
+
 createApp(App).use(ElementPlus).use(store).use(router).mount('#app')
